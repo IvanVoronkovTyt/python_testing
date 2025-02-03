00d565e1d6fa057e9f61128275facd25bb843e07
i.voronkov <i.voronkov@experium.ru>
2025-02-03 10:58:49 +0300

    add test

--- test_function.py
+++ test_function.py
@@ -60,33 +60,33 @@ def test_11():
 
 # Тесты на ограничения по типам данных
 @pytest.mark.parametrize('distance', ["фыв", "asd", "#!%", " "])
-def test_51(distance):
+def test_distance_limited_value(distance):
     text = delivery_cost_calculation(distance, "big", True, "increased")
     assert text == "Расстояния до пункта назначения может быть только целочисленное или число с плавающей точкой", \
         f"Текс равен '{text}'"
 
 
 @pytest.mark.parametrize('dimensions', [0, "фыв", "asd", "#!%", " "])
-def test_52(dimensions):
+def test_dimensions_limited_value(dimensions):
     text = delivery_cost_calculation(12, dimensions, True, "increased")
     assert text == "Габариты груза могут принимать только значения: big, small ", \
         f"Текс равен '{text}'"
 
 
 @pytest.mark.parametrize('fragile', [0, "фыв", "asd", "#!%", " "])
-def test_53(fragile):
+def test_fragile_limited_value(fragile):
     text = delivery_cost_calculation(13, "big", fragile, "increased")
     assert text == "Информацию о хрупкости груза может принимать только булевы значения", \
         f"Текс равен '{text}'"
 
 
 @pytest.mark.parametrize('workload', [0, "фыв", "asd", "#!%", " "])
-def test_54(workload):
+def test_workload_limited_value(workload):
     text = delivery_cost_calculation(13, "big", True, workload)
     assert text == "Информацию о загруженности службы доставки может принимать только значения:" \
                    " very high, high, increased, normal", f"Текс равен '{text}'"
 
 
-def test_55():
+def test_distance_for_fragile_cargo():
     text = delivery_cost_calculation(32, "big", True, "increased")
     assert text == "Хрупкие грузы нельзя возить на расстояние более 30 км.", f"Текс равен '{text}'"
